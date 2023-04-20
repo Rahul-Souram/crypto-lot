a63ca3f90a59e36841657061eea471a1a713b5be
rahul <rahul@ozz.tech>
2023-04-20 16:02:59 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import QuestionMark from "../src/svg/heart-circle.svg";
+import Reverse from "../src/svg/reverse.svg";
+import MemberIcon from "../src/svg/member-icon.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-body">
+      <div className="card-container">
+        <div className="card-header">
+          <div className="mostplayed">most Played</div>
+          <div>
+            <img src={QuestionMark} alt="question-mark" />
+          </div>
+        </div>
+        <div className="card-icons-section">
+          <div>
+            <img src={MemberIcon} alt="member-icon" />
+          </div>
+          <div>
+            <img src={Reverse} alt="reverse" />
+          </div>
+          <div>
+            <img src={MemberIcon} alt="member-icon" />
+          </div>
+        </div>
+        <div className="card-details-section">
+          <p className="title">Multi-user Lot</p>
+          <p className="tagline">
+            In this lot multiple people will bet against you. The condition for
+            the lot to start is both sides should have equal funds
+          </p>
+        </div>
+        <button type="button" className="card-btn">
+          Create Lot
+        </button>
+      </div>
     </div>
   );
 }
